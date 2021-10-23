e4500f116b30dbef18c461f18ae0402c15e87f57
unknown <angelaescariocorpuz@gmail.com>
2021-10-24 02:51:56 +0800

    second program under assignment no. 2

--- Program_2 (how many apples and oranges you want to buy).py
+++ Program_2 (how many apples and oranges you want to buy).py
@@ -1,3 +1,10 @@
 Numberofapples = int(input("Enter the number of apples that you want to buy: "))
 Numberoforanges = int(input("Enter the number of oranges that you want to buy: "))
 
+Priceofapple = 20
+Priceoforange = 25
+
+Totalamountofapples = Numberofapples * Priceofapple
+Totalamountoforanges = Numberoforanges * Priceoforange
+Total = Totalamountofapples + Totalamountoforanges
+print(f'')
